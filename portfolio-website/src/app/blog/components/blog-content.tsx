9a3e86340b953db35ab735ec469813fdbf1624b0
Ryan Loh <ryanloh29@gmail.com>
2023-12-20 22:27:41 +0800

    Change width ratio

--- portfolio-website/src/app/blog/components/blog-content.tsx
+++ portfolio-website/src/app/blog/components/blog-content.tsx
@@ -10,7 +10,7 @@ const BlogContent = () => {
 						<div
 							onClick={() => {}}
 							key={blog.title}
-							className="border hover:rounded-[8px] border-transparent dark:hover:border-gray-300 hover:border-gray-900 transition-all duration-300 w-[85%]  sm:w-4/5 md:w-2/5 flex justify-center text-start flex-col"
+							className="border hover:rounded-[8px] border-transparent dark:hover:border-gray-300 hover:border-gray-900 transition-all duration-300 w-[85%]  sm:w-4/5 md:w-3/5 flex justify-center text-start flex-col"
 						>
 							<a className="p-4" href={"blog/" + blog.href}>
 								<h1 className="text-[30px] mb-[1px]">{blog.title}</h1>
